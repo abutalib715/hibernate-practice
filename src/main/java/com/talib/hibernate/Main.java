b3eedc8f254713226c9097ccd85206713ad6a6cc
Talib <abutalib715@gmail.com>
2024-07-29 00:00:45 +0600

    xml based basic hibernate config

--- src/main/java/com/talib/hibernate/Main.java
+++ src/main/java/com/talib/hibernate/Main.java
@@ -1,7 +1,18 @@
 package com.talib.hibernate;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Project Running...");
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+//        configuration.configure("hibernate.cfg.xml"); // if file name or location customized
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        System.out.println(sessionFactory);
+        System.out.println(sessionFactory.isClosed());
     }
 }
