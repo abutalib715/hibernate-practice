b70364ad3a4cf1fe235aeb1b7d228e11864dd366
Talib <abutalib715@gmail.com>
2024-07-30 01:25:45 +0600

    student entity created

--- src/main/java/com/talib/hibernate/Main.java
+++ src/main/java/com/talib/hibernate/Main.java
@@ -1,6 +1,8 @@
 package com.talib.hibernate;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
@@ -10,9 +12,34 @@ public class Main {
         Configuration configuration = new Configuration();
         configuration.configure();
 //        configuration.configure("hibernate.cfg.xml"); // if file name or location customized
-
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-        System.out.println(sessionFactory);
-        System.out.println(sessionFactory.isClosed());
+
+        // CREATE NEW STUDENT OBJ
+        Student student = new Student();
+        student.setName("Lablu");
+        student.setCity("CTG");
+
+        // GET CURRENT SESSION AND SAVE DATA
+        Session session = null;
+        Transaction tx = null;
+
+        try {
+            session = sessionFactory.getCurrentSession();
+            tx = session.beginTransaction();
+            session.save(student);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+        // Close the SessionFactory
+        sessionFactory.close();
     }
 }
